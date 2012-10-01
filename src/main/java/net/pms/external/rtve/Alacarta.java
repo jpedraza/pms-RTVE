f046d638c6db20a69b18824db69dded80f24c831
danighieri <danichaves@gmail.com>
2012-10-01 23:26:41 +0200

    Fix category 'infantiles'

--- src/main/java/net/pms/external/rtve/Alacarta.java
+++ src/main/java/net/pms/external/rtve/Alacarta.java
@@ -25,6 +25,8 @@ import net.pms.external.rtve.response.Response;
 import net.pms.external.rtve.response.Url;
 import net.pms.network.HTTPResource;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
 
 public class Alacarta extends HTTPResource {
 
@@ -45,20 +47,23 @@ public class Alacarta extends HTTPResource {
         SecretKeySpec ks = new SecretKeySpec(KEY.getBytes(), "Blowfish");
         cipher.init(Cipher.ENCRYPT_MODE, ks);
         String cleartext = assetId + "_" + mediatype + "_" + lang;
-        String b64textCleartext = Base64.encodeBase64String(cipher.doFinal(cleartext.getBytes()));
-        byte data[] = downloadAndSendBinary(URL_PREFIX + b64textCleartext);
-        String b64textResponse = new String(data, "UTF-8");
-        byte[] ciphertext = Base64.decodeBase64(b64textResponse.getBytes());
+        String b64textReq = Base64.encodeBase64String(cipher.doFinal(cleartext.getBytes()));
+        byte data[] = downloadAndSendBinary(URL_PREFIX + b64textReq);
+        String b64textRes = new String(data, "UTF-8");
+        byte[] ciphertext = Base64.decodeBase64(b64textRes.getBytes());
         cipher.init(Cipher.DECRYPT_MODE, ks);
         InputStream xml = new ByteArrayInputStream(cipher.doFinal(ciphertext));
         JAXBContext jc = JAXBContext.newInstance(Response.class);
         Response response = (Response) jc.createUnmarshaller().unmarshal(xml);
         if (response != null && response.getCode().equalsIgnoreCase("OK")) {
-            for (Url url : response.getUrl()) {
-                if (!url.getTipo().equals("failover") && url.getProvider().contains("AKAMAI")) {
-                    return url.getValue();
+            Url url = (Url) CollectionUtils.find(response.getUrl(), new Predicate() {
+                @Override
+                public boolean evaluate(Object o) {
+                    Url u = (Url) o;
+                    return u.getTipo().equals("failover");
                 }
-            }
+            });
+            return url.getValue();
         }
         return null;
     }
